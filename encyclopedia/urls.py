fff15b507a116ee125398f9ab3d67a19b479cbe2
Ashish <landxcape09@gmail.com>
2020-09-23 14:49:38 +0545

    added search functionality

--- encyclopedia/urls.py
+++ encyclopedia/urls.py
@@ -5,5 +5,5 @@ from . import views
 urlpatterns = [
     path("", views.index, name="index"),
     path("wiki/<str:title>", views.wiki, name="wiki"),
-
+    path("results", views.results, name="results")
 ]
--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -1,4 +1,8 @@
+import re
+
 from django.shortcuts import render
+from django.http import HttpResponseRedirect
+from django.urls import reverse
 
 from . import util
 
@@ -13,4 +17,16 @@ def wiki(request, title):
     return render(request, "encyclopedia/wiki.html", {
         "title": title,
         "entry": util.get_entry(title)
+    })
+
+
+def results(request):
+    query = request.GET["q"]
+    result = re.compile(query, re.I)
+    results = list(filter(result.search, util.list_entries()))
+    if len(results) == 1 and re.match('\\b' + query + '\\b', results[0], re.I):
+        return HttpResponseRedirect(reverse("wiki", args=results))
+    return render(request, "encyclopedia/results.html", {
+        "q": query,
+        "results": results
     })
